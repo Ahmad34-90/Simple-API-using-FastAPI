8ec740db452ad7efd147225f7af9be7274b82692
Muhammad Ahmad <ahmiiikha@gmail.com>
2026-03-17 10:47:12 +0500

    Created  endpoint points as a tasks

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from fastapi import FastAPI
 app =FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message":"Hello World!"}
+async def home():
+    return {"message":"That is home endpoint"}
+
+@app.get("/about")
+async def about():
+    return {"message": "taht is About Endpint"}
+
+@app.get("/contact")
+async def contact():
+    return {"message": "that is Contact endpoint"}
